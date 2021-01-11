4875c7b73e0117086fcefc7f954e804b19bbcccc
kevin <kevinhellman94@gmail.com>
2021-01-11 11:31:52 -0500

    Added local storage set and get

--- myscript.js
+++ myscript.js
@@ -1,8 +1,25 @@
-$("#lock").click(function(event) {
+getCity();
+
+$("#search").click(function(event) {
     event.preventDefault();
     var nameOfCity = $("#cityName").val();
+    addCity(nameOfCity);
+});
+function addCity(cityName) {
     var name = $("<p>");
-    name.text(nameOfCity);
+    name.text(cityName);
     name.attr("class","city");
     $(".addCity").append(name);
-});
+    storeCity(cityName);
+}
+function storeCity(cityName) {
+    localStorage.setItem("cityName",JSON.stringify(cityName));
+}
+
+function getCity() {
+    var lastCity = JSON.parse(localStorage.getItem("cityName"));
+    if (lastCity !== null) {
+    addCity(lastCity);
+    }
+
+}
