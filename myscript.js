efcea88bc3b234454d4db4574645fc6c586179cc
kevin <kevinhellman94@gmail.com>
2021-01-11 15:40:38 -0500

    Added Functional UV Index

--- myscript.js
+++ myscript.js
@@ -1,18 +1,20 @@
 getCity();
+var latitude = 0;
+var longitude = 0;
 
-$("#search").click(function() {
+$("#search").click(function () {
     var nameOfCity = $("#cityName").val();
     addCity(nameOfCity);
     cityInfo(nameOfCity);
 });
-$(document).on("click",".city",function() {
+$(document).on("click", ".city", function () {
     var getCity = $(this).text();
     cityInfo(getCity);
 });
 function addCity(cityName) {
     var name = $("<button>");
     name.text(cityName);
-    name.attr("class","city");
+    name.attr("class", "city");
     $(".addCity").append(name);
     storeCity(cityName);
 }
@@ -20,39 +22,63 @@ function cityInfo(thisCity) {
     var addHere = $("#displayName");
     addHere.text(thisCity);
 
-    var queryURLCity = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q="+thisCity+"&appid=3aedaa4c3534c982b48f6a82a6ddb305";
-    var queryURL5Day = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q="+thisCity+"&appid=3aedaa4c3534c982b48f6a82a6ddb305";
+    var queryURLCity = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=" + thisCity + "&appid=3aedaa4c3534c982b48f6a82a6ddb305";
+    var queryURL5Day = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=" + thisCity + "&appid=3aedaa4c3534c982b48f6a82a6ddb305";
+    
 
     $.ajax({
         url: queryURLCity,
         type: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
         var temperatureK = response.main.temp;
         var humidity = response.main.humidity;
         var windSpeed = response.wind.speed;
-        var temperatureF = ((temperatureK-273.15)*9/5+32).toFixed(2);
+        var temperatureF = ((temperatureK - 273.15) * 9 / 5 + 32).toFixed(2);
+        var cityLat = response.coord.lat;
+        var cityLong = response.coord.lon;
+        latitude = cityLat;
+        longitude = cityLong;
         $("#temperature").text("Temperature: " + temperatureF);
         $("#humidity").text("Humidity: " + humidity + " %");
         $("#windSpeed").text("Wind Speed: " + windSpeed + " MPH");
+        console.log(latitude);
+
+        $.ajax({
+            url: queryURL5Day,
+            type: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            var queryURLUV = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=3aedaa4c3534c982b48f6a82a6ddb305";
+
+            $.ajax({
+                url: queryURLUV,
+                type: "GET"
+            }).then(function (response) {
+                console.log(response);
+                console.log(latitude);
+                var UV = response.value;
+                $("#UVIndex").text("UV Index: " + UV);
+            });
+            
+        });
+
     
     });
 
-    $.ajax({
-        url: queryURL5Day,
-        type: "GET"
-    }).then(function(response) {
-        console.log(response);
-    })
+    
+
+
 }
 function storeCity(cityName) {
-    localStorage.setItem("cityName",JSON.stringify(cityName));
+    localStorage.setItem("cityName", JSON.stringify(cityName));
 }
 
 function getCity() {
     var lastCity = JSON.parse(localStorage.getItem("cityName"));
     if (lastCity !== null) {
-    addCity(lastCity);
+        addCity(lastCity);
     }
 
 }
